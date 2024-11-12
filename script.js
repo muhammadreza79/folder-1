94e6908902844a1acc48dbc63bb8b6e9cd936838
muhammadreza <ghasemsolo1@gmail.com>
2024-11-12 12:09:49 +0330

    js with functions updated

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 let ulElem = document.querySelector('ul');
 let liElem = document.querySelector('li');
+
+let $ = document;
+let headerElem= $.querySelector('header');
+liElem.addEventListener('click',function(){
+    alert('please check your internet connection')
+})
